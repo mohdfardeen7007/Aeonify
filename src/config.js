f161c5cdaee587dac97496bc67d0391f1b0fa381
mohdfardeen7007 <162273323+mohdfardeen7007@users.noreply.github.com>
2025-06-29 13:03:22 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,15 +3,15 @@ dotenv.config();
 
 const config = {
 
-  mongoUrl: process.env.MONGODB_URI || '',
+  mongoUrl: process.env.MONGODB_URI || 'mongodb+srv://vajira:vajira@cluster0.hzwqrqq.mongodb.net/?retryWrites=true&w=majority',
   mongoOptions: {
     serverSelectionTimeoutMS: 5000,
     socketTimeoutMS: 45000,
   },
 
-  botName: process.env.BOT_NAME || "Aeonify",
-  ownerNumber: process.env.OWNER_NUMBERS ? process.env.OWNER_NUMBERS.split(",") : ['916297175943'],
-  ownerName: process.env.OWNER_NAME || "Aeon",
+  botName: process.env.BOT_NAME || "𝐅𝖾ᥣ𝗂𝗑",
+  ownerNumber: process.env.OWNER_NUMBERS ? process.env.OWNER_NUMBERS.split(",") : ['918127875972'],
+  ownerName: process.env.OWNER_NAME || "𝐅𝖾ᥣ𝗂𝗑",
   prefix: process.env.PREFIX || "!",
 
   sessionId: process.env.SESSION_ID || "",
